2a5c0773ab581445509d39e3863d92213116a450
hon2y <hon222y@gmail.com>
2014-10-02 16:28:09 +0900

    yy

--- sketch.js
+++ sketch.js
@@ -12,9 +12,17 @@ function draw () {
     background(50, 0, 50);
 	ellipse(100, 100, 100, 100)
 // draw function
-if (mouseX < 300) {
-  ellipse(200, 200, 100, 100);
+if (mouseX < 100) {
+  ellipse(50, 200, 100, 100);
+} else if (mouseX < 200){
+  ellipse(150, 200, 100, 100);
+} else if (mouseX < 300){
+  ellipse(250, 200, 100, 100);
+} else if (mouseX < 400){
+  ellipse(350, 200, 100, 100);
+} else if (mouseX < 500){
+  ellipse(450, 200, 100, 100);
 } else {
-  ellipse(400, 200, 100, 100);
+  ellipse(550, 200, 100, 100);
 }
 }
