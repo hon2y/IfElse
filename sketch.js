80fe47da9a8a6df88166c67f9d4c641ccfd6cbb7
hon2y <hon222y@gmail.com>
2014-10-02 16:42:30 +0900

    xx

--- sketch.js
+++ sketch.js
@@ -3,26 +3,38 @@ function setup() {
   
   noFill();
 
-  stroke(255, 0, 0);
+  stroke(255, 255, 255);
 
 }
 
 function draw () {
 
-    background(50, 0, 50);
-	ellipse(100, 100, 100, 100)
+    background(50, 100, 50);
+	ellipse(320, 240, 100, 100)
 // draw function
-if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
-} else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
-} else if (mouseX < 300){
-  ellipse(250, 200, 100, 100);
-} else if (mouseX < 400){
-  ellipse(350, 200, 100, 100);
-} else if (mouseX < 500){
-  ellipse(450, 200, 100, 100);
+if (mouseY < 0) {
+  ellipse(0, 0, 30, 30);
+} else if (mouseY < 50){
+  ellipse(50, 50, 50, 50);
+} else if (mouseY < 100){
+  ellipse(100, 100, 70, 70);
+} else if (mouseY < 150){
+  ellipse(150, 150, 90, 90);
+} else if (mouseY < 200){
+  ellipse(200, 200, 110, 110);
+} else if (mouseY < 250){
+  ellipse(250, 250, 130, 130);
+  } else if (mouseY < 300){
+  ellipse(300, 300, 150, 150);
+} else if (mouseY < 350){
+  ellipse(400, 400, 170, 170);
+} else if (mouseY < 400){
+  ellipse(450, 450, 190, 190);
+} else if (mouseY < 450){
+  ellipse(550, 200, 210, 210);
+} else if (mouseY < 500){
+  ellipse(650, 100, 230, 230);
 } else {
-  ellipse(550, 200, 100, 100);
+  ellipse(750, 0, 250, 250)
 }
 }
