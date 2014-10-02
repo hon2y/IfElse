11bf85ad01a7be4fcc392d2f159eea9fe24c6810
hon2y <hon222y@gmail.com>
2014-10-02 16:59:55 +0900

    tt

--- sketch.js
+++ sketch.js
@@ -13,28 +13,154 @@ function draw () {
 	ellipse(320, 240, 100, 100)
 // draw function
 if (mouseY < 0) {
+  fill(255, 255, 255, 30)
   ellipse(0, 0, 30, 30);
 } else if (mouseY < 50){
+	fill(255, 255, 255, 50)
   ellipse(50, 50, 50, 50);
 } else if (mouseY < 100){
+	fill(255, 255, 255, 70)
   ellipse(100, 100, 70, 70);
 } else if (mouseY < 150){
+	fill(255, 255, 255, 90)
   ellipse(150, 150, 90, 90);
 } else if (mouseY < 200){
+	fill(255, 255, 255, 110)
   ellipse(200, 200, 110, 110);
 } else if (mouseY < 250){
+	fill(255, 255, 255, 130)
   ellipse(250, 250, 130, 130);
   } else if (mouseY < 300){
+  	fill(255, 255, 255, 150)
   ellipse(300, 300, 150, 150);
 } else if (mouseY < 350){
+	fill(255, 255, 255, 170)
   ellipse(400, 400, 170, 170);
 } else if (mouseY < 400){
+	fill(255, 255, 255, 190)
   ellipse(450, 450, 190, 190);
 } else if (mouseY < 450){
-  ellipse(550, 200, 210, 210);
+	fill(255, 255, 255, 210)
+  ellipse(550, 300, 210, 210);
 } else if (mouseY < 500){
-  ellipse(650, 100, 230, 230);
+	fill(255, 255, 255, 230)
+  ellipse(650, 200, 230, 230);
 } else {
-  ellipse(750, 0, 250, 250)
+	fill(255, 255, 255, 255)
+  ellipse(750, 100, 250, 250)
+}
+
+if (mouseY < 50) {
+  fill(230, 230, 230, 20)
+  ellipse(0, 0, 30, 30);
+} else if (mouseY < 100){
+	fill(230, 230, 230, 40)
+  ellipse(50, 50, 50, 50);
+} else if (mouseY < 150){
+	fill(230, 230, 230, 60)
+  ellipse(100, 100, 70, 70);
+} else if (mouseY < 200){
+	fill(230, 230, 230, 80)
+  ellipse(150, 150, 90, 90);
+} else if (mouseY < 250){
+	fill(230, 230, 230, 100)
+  ellipse(200, 200, 110, 110);
+} else if (mouseY < 300){
+	fill(230, 230, 230, 120)
+  ellipse(250, 250, 130, 130);
+  } else if (mouseY < 350){
+  	fill(230, 230, 230, 140)
+  ellipse(300, 300, 150, 150);
+} else if (mouseY < 400){
+	fill(230, 230, 230, 160)
+  ellipse(400, 400, 170, 170);
+} else if (mouseY < 450){
+	fill(230, 230, 230, 180)
+  ellipse(450, 450, 190, 190);
+} else if (mouseY < 500){
+	fill(230, 230, 230, 200)
+  ellipse(550, 300, 210, 210);
+} else if (mouseY < 550){
+	fill(230, 230, 230, 220)
+  ellipse(650, 200, 230, 230);
+} else {
+	fill(230, 230, 230, 240)
+  ellipse(750, 100, 250, 250)
+}
+
+if (mouseY < 100) {
+  fill(200, 200, 200, 10)
+  ellipse(0, 0, 30, 30);
+} else if (mouseY < 150){
+	fill(200, 200, 200, 30)
+  ellipse(50, 50, 50, 50);
+} else if (mouseY < 200){
+	fill(200, 200, 200, 50)
+  ellipse(100, 100, 70, 70);
+} else if (mouseY < 250){
+	fill(200, 200, 200, 70)
+  ellipse(150, 150, 90, 90);
+} else if (mouseY < 300){
+	fill(200, 200, 200, 90)
+  ellipse(200, 200, 110, 110);
+} else if (mouseY < 350){
+	fill(200, 200, 200, 110)
+  ellipse(250, 250, 130, 130);
+  } else if (mouseY < 400){
+  	fill(200, 200, 200, 130)
+  ellipse(300, 300, 150, 150);
+} else if (mouseY < 450){
+	fill(200, 200, 200, 150)
+  ellipse(400, 400, 170, 170);
+} else if (mouseY < 500){
+	fill(200, 200, 200, 170)
+  ellipse(450, 450, 190, 190);
+} else if (mouseY < 550){
+	fill(200, 200, 200, 190)
+  ellipse(550, 300, 210, 210);
+} else if (mouseY < 600){
+	fill(200, 200, 200, 210)
+  ellipse(650, 200, 230, 230);
+} else {
+	fill(200, 200, 200, 230)
+  ellipse(750, 100, 250, 250)
+}
+
+if (mouseY < 15) {
+  fill(170, 170, 170, 0)
+  ellipse(0, 0, 30, 30);
+} else if (mouseY < 200){
+	fill(170, 170, 170, 20)
+  ellipse(50, 50, 50, 50);
+} else if (mouseY < 250){
+	fill(170, 170, 170, 40)
+  ellipse(100, 100, 70, 70);
+} else if (mouseY < 300){
+	fill(170, 170, 170, 60)
+  ellipse(150, 150, 90, 90);
+} else if (mouseY < 350){
+	fill(170, 170, 170, 80)
+  ellipse(200, 200, 110, 110);
+} else if (mouseY < 400){
+	fill(170, 170, 170, 100)
+  ellipse(250, 250, 130, 130);
+  } else if (mouseY < 450){
+  	fill(170, 170, 170, 120)
+  ellipse(300, 300, 150, 150);
+} else if (mouseY < 500){
+	fill(170, 170, 170, 140)
+  ellipse(400, 400, 170, 170);
+} else if (mouseY < 550){
+	fill(170, 170, 170, 160)
+  ellipse(450, 450, 190, 190);
+} else if (mouseY < 600){
+	fill(170, 170, 170, 180)
+  ellipse(550, 300, 210, 210);
+} else if (mouseY < 650){
+	fill(170, 170, 170, 200)
+  ellipse(650, 200, 230, 230);
+} else {
+	fill(170, 170, 170, 220)
+  ellipse(750, 100, 250, 250)
 }
 }
